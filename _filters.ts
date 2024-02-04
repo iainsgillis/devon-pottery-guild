a5635ebf1c087b6c8797807795f4be153007f2fc
Iain S Gillis <iain@iainsgillis.com>
2024-02-04 15:29:57 -0700

    add Event type

--- _filters.ts
+++ _filters.ts
@@ -1,9 +1,19 @@
-export function getFutureEvents(events) {
+type Event = {
+  title: string;
+  where: string;
+  poster: string;
+  times: Array<{
+    start: string;
+    end: string;
+  }>;
+};
+
+export function getFutureEvents(events: Event[]) {
   const today = new Date()
   return events.filter(event => event.times.every(time => today < (new Date(time.end))))
 }
 
-export function getPastEvents(events) {
+export function getPastEvents(events: Event[]) {
   const today = new Date()
   return events.filter(event => event.times.every(time => (new Date(time.end)) < today))
 }
