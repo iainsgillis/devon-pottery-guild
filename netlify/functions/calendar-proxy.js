798ec05d7a282c5740f9cf3dd1c7a39305251c48
iainsgillis <iainsgillis@users.noreply.github.com>
2024-03-30 13:40:09 -0600

    add date filter via query params

--- netlify/functions/calendar-proxy.js
+++ netlify/functions/calendar-proxy.js
@@ -1,8 +1,9 @@
 exports.handler = async (event, context) => {
-	const GOOGLE_CALENDAR_API_KEY = process.env.GOOGLE_CALENDAR_API_KEY;
+    const { startDate, endDate } = event.queryStringParameters;
+    const GOOGLE_CALENDAR_API_KEY = process.env.GOOGLE_CALENDAR_API_KEY;
 	const calendarId = "devonpottery@gmail.com";
-	const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${GOOGLE_CALENDAR_API_KEY}`;
-
+	const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${GOOGLE_CALENDAR_API_KEY}&singleEvents=true&eventTypes=default&eventTypes=focusTime&eventTypes=outOfOffice&timeZone=America%2FEdmonton&maxResults=250&sanitizeHtml=true&timeMin=${startDate}T00%3A00%3A00-06%3A00&timeMax=${endDate}T00%3A00%3A00-06%3A00`;
+    
 	try {
 		const response = await fetch(url);
 		const data = await response.json();
